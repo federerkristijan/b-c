2682cc8ae500eb2d9bdc9368bc50584dfcd374fe
federerkristijan <federer.kristijan@gmail.com>
2023-01-31 22:54:25 +0100

    global.css

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
-import '@/styles/globals.css'
+import Link from "next/link";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <nav>
+        <Link href="/">
+          Burner Cookbook
+        </Link>
+      </nav>
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
