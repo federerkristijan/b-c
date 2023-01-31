8d2608f52a55ff16e1e396b4fe68c11d2481584f
federerkristijan <federer.kristijan@gmail.com>
2023-01-31 21:54:17 +0100

    recipe.js bug fixed

--- studio/schemas/index.js
+++ studio/schemas/index.js
@@ -1,4 +1,5 @@
 import chef from "./chef";
 import ingredient from "./ingredient";
+import recipe from "./recipe";
 
-export const schemaTypes = [chef, ingredient]
+export const schemaTypes = [chef, ingredient, recipe]
