1734af2cc6d6d15f2a6bfefa7c20ba45f5dc8d96
federerkristijan <federer.kristijan@gmail.com>
2023-02-01 15:18:04 +0100

    work in process on lib/client.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,20 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "@/styles/Home.module.css";
+import Link from "next/link";
+import { sanityClient, urlFor } from "@/lib/sanity";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-export default function Home({data}) {
-  const recipes = data.recipes;
+const recipesQuery = `*[_type == "recipe"]{
+  _id,
+  name,
+  slug,
+  mainImage
+}`;
 
+export default function Home({ recipes }) {
   return (
     <>
       <Head>
@@ -18,24 +25,30 @@ export default function Home({data}) {
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
-          <div>
-            <a
-              href="/about"
-            >{recipes[0].title}
-            </a>
-          </div>
+          <h1>Welcome to the burner cookbook</h1>
+          <ul>
+            {recipes?.length > 0 && recipes.map((recipe) =>
+            (
+            <li key={recipe._id}>
+              <Link href="/about">
+                <Image src={urlFor(recipe.mainImage).url()} alt="" width={250} height={250} />
+                <span></span>
+              </Link>
+            </li>
+            ))}
+          </ul>
         </div>
       </main>
     </>
-  )
+  );
 }
 
-export function getStaticProps() {
+export async function getStaticProps() {
+  const recipes = await sanityClient.fetch(recipesQuery);
+
   return {
     props: {
-      data: {
-        recipes: [{ title: "Vegan Curry"}]
-      }
-    }
-  }
+      recipes,
+    },
+  };
 }
