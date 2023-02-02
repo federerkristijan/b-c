e95031e26010594419263be8418b11b923d34a0d
federerkristijan <federer.kristijan@gmail.com>
2023-02-02 01:09:23 +0100

    getStaticPaths & getStaticProps

--- pages/reciipes/[slug].js
+++ pages/reciipes/[slug].js
@@ -17,11 +17,37 @@ const recipeQuery = `*[_type == "recipe" && slug.current == $slug][0]{
     ingredient->{
       name
     }
-  }
+  },
+  instructions
 }`;
 
-export default function OneRecipe() {};
+export default function OneRecipe() {}
 
-export async function getStaticPaths() {};
+export async function getStaticPaths() {
+  const pats = await sanityClient.fetch(
+    `*[_typ == "recipe" && defined(slug.current)]{
+      "params": {
+        "slug": slug.current
+      }
+    }`
+  );
 
-export async function getStaticProps() {};
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const { slug } = params;
+
+  const recipe = await sanityClient.fetch(recipeQuery, { slug });
+
+  return {
+    props: {
+      data: {
+        recipe,
+      },
+    },
+  };
+}
