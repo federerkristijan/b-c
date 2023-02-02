968c56910bf30c07eca0679096494372139579b5
federerkristijan <federer.kristijan@gmail.com>
2023-02-02 01:01:55 +0100

    [slug].js -> recipeQuery

--- pages/reciipes/[slug].js
+++ pages/reciipes/[slug].js
@@ -1,6 +1,27 @@
 import { sanityClient, urlFor, PortableText } from "../../lib/sanity";
 
-const recipeQuery = ``;
+const recipeQuery = `*[_type == "recipe" && slug.current == $slug][0]{
+  _id,
+  name,
+  slug,
+  mainImage{
+    asset->{
+      _id,
+      url
+    }
+  },
+  ingredient[]{
+    unit,
+    wholeNumber,
+    fraction,
+    ingredient->{
+      name
+    }
+  }
+}`;
 
 export default function OneRecipe() {};
 
+export async function getStaticPaths() {};
+
+export async function getStaticProps() {};
